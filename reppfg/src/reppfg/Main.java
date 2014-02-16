c800cad4262c0759e79e26d6024bc9958684dc55
David <davidrbermejo@outlook.com>
2014-02-16 21:22:27 +0000

    Primer commit en condiciones

--- reppfg/src/reppfg/Main.java
+++ reppfg/src/reppfg/Main.java
@@ -1,11 +1,19 @@
 package reppfg;
 
+import javax.swing.JFrame;
+
+import reppfg.Solomon;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-
-	}
+	   public static void main( String args[] )
+	   {
+	      Solomon solo = new Solomon();
+	      solo.lectura();
+	      //representar aplicacion = new representar();
+		  //Puntos p = new Puntos(); 
+	      //p.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+	      
+	   }
 
 }
